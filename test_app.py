29fe7c9bf896048e3eb76a4849c30eab434fbd85
kishorekanna12 <kishorekanna09052007@gmail.com>
2026-09-24 10:53:26 +0530

    Clean
    Removed comments indicating the number of tests for square and is_even functions.

--- test_app.py
+++ test_app.py
@@ -1,13 +1,13 @@
 from app import square, is_even
 
-# Two tests for square(n)
+
 def test_square_positive():
     assert square(4) == 16
 
 def test_square_negative():
     assert square(-3) == 9
 
-# Two tests for is_even(n)
+
 def test_is_even_true():
     assert is_even(4) is True
 
