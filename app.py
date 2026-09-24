24ef4d618c2cdbf6e14245ed3054c8ed684fe7b7
kishorekanna12 <kishorekanna09052007@gmail.com>
2026-09-24 10:55:26 +0530

    update
    Removed docstrings from square and is_even functions.

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 def square(n):
-    """Returns the square of a number."""
+    
     return n * n
 
 def is_even(n):
-    """Returns True if a number is even, False otherwise."""
+    
     return n % 2 == 0
